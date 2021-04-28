aca909d1125feec7304984d005beb1f77a7879ff
jacgoldsm <54956846+jacgoldsm@users.noreply.github.com>
2021-04-28 07:45:42 -0700

    Update chewsday.R

--- chewsday.R
+++ chewsday.R
@@ -1,8 +1,8 @@
 library(twitteR)
 
-setup_twitter_oauth(consumer_key = "bfULkb1d3JI3XfTDQlmph2Z9O",
-                    access_token = "1387406382526210060-tLxmXGCmiNhpdxsMqJwlqgUriGgtMe",
-                    consumer_secret = "HxfbRiBuvpI6Q4xjo0AbhxB1w9uCrG3DI2nviftby5rocaMIuX",
-                    access_secret = "zXSWv8yJHVDdLGUfrvsJrgZnekwPBh14kVKbLKjYXmFjX")
+setup_twitter_oauth(consumer_key = "",
+                    access_token = "",
+                    consumer_secret = "",
+                    access_secret = "")
 
 tw <- updateStatus("Oi, it's Chewsday, innit?")
